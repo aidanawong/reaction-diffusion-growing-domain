0e4205fd97edb87997cb1618385b20f2f9dd6941
Punthano <aidan.ck.wong@gmail.com>
2024-05-16 14:59:18 -0600

    Changed main for Schnakenberg model

--- code/__main__.py
+++ code/__main__.py
@@ -3,13 +3,13 @@ from solve_pde import ReactDiffusion
 
 def main():
 
-    width = 4.8019
-    D, R, init = reaction_func.tbv_model_dim()
+    width = 1
+    D, R, init = reaction_func.schnak()
     dt = 0.1
     dx = 0.01 
-    T = 840
+    T = 800
     
-    r, rt, radius = grow_func.growbackward(width=width, time_mult=T / 6.35)
+    r, rt = grow_func.growlog()
 
     steps = 70
     n_steps = round(T / (steps * dt))
@@ -19,6 +19,6 @@ def main():
         ic=init, bc="neumann",
         width=width, dx=dx, dt=dt,
         steps=steps, labels=["Tulip Pigments", "Building Blocks", "Virus"], cmaps=["PuRd", "Greens", "Blues"]
-    ).plot_tri("figures/test.png", n_steps=n_steps, symmetry=True, radius=radius, flipx=True, flipy=False)
+    ).plot_tri("reaction-diffusion-growing-domain/figures/test.png", n_steps=n_steps, symmetry=False, radius=None, flipx=False, flipy=False)
 
 main()
