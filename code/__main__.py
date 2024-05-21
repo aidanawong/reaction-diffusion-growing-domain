b2422576ef8c40b95740092d2338ebd2bac9bbd2
Punthano <aidan.ck.wong@gmail.com>
2024-05-20 23:38:54 -0600

    Commented main

--- code/__main__.py
+++ code/__main__.py
@@ -1,4 +1,12 @@
+"""
+Reaction Diffusion Equation on Growing 1D Domain
+User interface for calculating PDEs
+Author: Aidan Wong
+Date: May 20, 2024
+"""
+
 from resources import grow_func, reaction_func
+from resources.grow_func import ORIGINAL_PETAL_LENGTH
 from solve_pde import ReactDiffusion
 
 def main():
@@ -9,7 +17,7 @@ def main():
     dx = 0.01 
     T = 800
     
-    r, rt, radius = grow_func.growforward(width=width, time_mult=T/6.35)
+    r, rt, radius = grow_func.growforward(width=width, time_mult=T/ORIGINAL_PETAL_LENGTH)
 
     steps = 10
     n_steps = round(T / (steps * dt))
@@ -19,6 +27,7 @@ def main():
         ic=init, bc="neumann",
         width=width, dx=dx, dt=dt,
         steps=steps, labels=["Tulip Pigments", "Building Blocks", "Virus"], cmaps=["PuRd", "Greens", "Blues"]
-    ).plot_tri("reaction-diffusion-growing-domain/figures/testo.png", n_steps=n_steps, symmetry=True, radius=radius, flipx=False, flipy=False)
+    ).plot_tri("reaction-diffusion-growing-domain/figures/testo.png", 
+               n_steps=n_steps, symmetry=True, radius=radius, flipx=False, flipy=False)
 
 main()
